58865746d7b82fac398045f31724f6d1931379fe
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-04-09 15:36:40 -0300

    padronizando os erros

--- Controllers/CategoryControler.cs
+++ Controllers/CategoryControler.cs
@@ -16,11 +16,11 @@ namespace Blog.Controllers
             {
                 var categories = await context.Categories.ToListAsync();
 
-                return Ok(categories);
+                return Ok(new ResultViewModel<List<Category>>(categories));
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "05XE13 - Falha interna no servidor");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE13 - Falha interna no servidor"));
             }
         }
 
@@ -30,18 +30,22 @@ namespace Blog.Controllers
             try
             {
                 var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-                if (category == null) return NotFound();
-                return Ok(category);
+
+                if (category == null) return NotFound(new ResultViewModel<Category>("Conteúdo não encontrado"));
+
+                return Ok(new ResultViewModel<Category>(category));
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "05XE15 - Falha interna no servidor");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE15 - Falha interna no servidor"));
             }
         }
 
         [HttpPost("v1/categories")]
         public async Task<IActionResult> PostAsync([FromBody] EditorCategoryViewModel model, [FromServices] BlogDataContext context)
         {
+            //if (!ModelState.IsValid) return BadRequest(ModelState.GetErrors());
+
             try
             {
                 var category = new Category(model.Name, model.Slug.ToLower());
@@ -52,11 +56,11 @@ namespace Blog.Controllers
             }
             catch (DbUpdateException ex)
             {
-                return StatusCode(500, "05XE9 - Não foi possível incluir a categoria");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE9 - Não foi possível incluir a categoria"));
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "05XE10 - Falha interna no servidor");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE10 - Falha interna no servidor"));
             }
         }
 
@@ -75,15 +79,15 @@ namespace Blog.Controllers
                 context.Categories.Update(category);
                 await context.SaveChangesAsync();
 
-                return Ok(category);
+                return Ok(new ResultViewModel<Category>(category));
             }
             catch (DbUpdateException ex)
             {
-                return StatusCode(500, "05XE7 - Não foi possível atualizar a categoria");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE7 - Não foi possível atualizar a categoria"));
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "05XE8 - Falha interna no servidor");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE8 - Falha interna no servidor"));
             }
         }
 
@@ -98,16 +102,16 @@ namespace Blog.Controllers
 
                 context.Categories.Remove(category);
                 await context.SaveChangesAsync();
-                return Ok(category);
+                return Ok(new ResultViewModel<Category>(category));
 
             }
             catch (DbUpdateException ex)
             {
-                return StatusCode(500, "05XE5 - Não foi possível remover a categoria");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE5 - Não foi possível remover a categoria"));
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "05XE6 - Falha interna no servidor");
+                return StatusCode(500, new ResultViewModel<List<Category>>("05XE6 - Falha interna no servidor"));
             }
         }
     }
---
+++ Extensions/ModelStateExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blog.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            return null;
+        }
+    }
+}
