7690ee45540d3acd92379762ec8f33d113706e09
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-04-25 16:11:10 -0300

    add authentication and authorization

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 ﻿using Blog.Models;
 using Blog.Services;
 using Blog.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
+    [Authorize]
     [ApiController]
     public class AccountController : ControllerBase
     {
@@ -15,6 +17,7 @@ namespace Blog.Controllers
             _tokenService = tokenService;
         }
 
+        [AllowAnonymous]
         [HttpPost("v1/login")]
         public IActionResult Login()
         {
@@ -29,5 +32,14 @@ namespace Blog.Controllers
                 return StatusCode(500, new ResultViewModel<List<Category>>("05XE13 - Falha interna no servidor"));
             }
         }
+
+        [HttpGet("v1/user")]
+        public IActionResult GetUser() => Ok(User.Identity.Name);
+
+        [HttpGet("v1/author")]
+        public IActionResult GetAuthor() => Ok(User.Identity.Name);
+
+        [HttpGet("v1/admin")]
+        public IActionResult GetAdmin() => Ok(User.Identity.Name);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
+using Blog;
 using Blog.Data;
 using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder
     .Services
@@ -11,9 +31,17 @@ builder
         options.SuppressModelStateInvalidFilter = true;
     });
 builder.Services.AddDbContext<BlogDataContext>();
+
+//AddTransient - toda vez que for invocado o objeto será inicializado
+//AddScoped - o objeto será inicializado uma vez no começo da requisição e finalizado ao final
+//AddSingleton - o objeto será inicializado apenas uma vez e assim ficará por toda a aplicação
 builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
