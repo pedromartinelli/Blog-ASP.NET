edce1fc4f058ee395033f77f3468479cd4a4d207
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-04-26 08:33:46 -0300

    wip

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
-    [Authorize]
     [ApiController]
     public class AccountController : ControllerBase
     {
@@ -17,7 +16,6 @@ namespace Blog.Controllers
             _tokenService = tokenService;
         }
 
-        [AllowAnonymous]
         [HttpPost("v1/login")]
         public IActionResult Login()
         {
@@ -33,12 +31,15 @@ namespace Blog.Controllers
             }
         }
 
+        [Authorize(Roles = "user")]
         [HttpGet("v1/user")]
         public IActionResult GetUser() => Ok(User.Identity.Name);
 
+        [Authorize(Roles = "author")]
         [HttpGet("v1/author")]
         public IActionResult GetAuthor() => Ok(User.Identity.Name);
 
+        [Authorize(Roles = "admin")]
         [HttpGet("v1/admin")]
         public IActionResult GetAdmin() => Ok(User.Identity.Name);
     }
