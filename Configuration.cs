b8a8daab2f9670884158659ade5a7caac4ea6901
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-04-30 11:57:04 -0300

    configurando appsettings e organizando o program.cs

--- Configuration.cs
+++ Configuration.cs
@@ -2,8 +2,17 @@
 {
     public static class Configuration
     {
-        public static string JwtKey = "YWM0Mzk2NzQtNGE0NC00ODA5LTgyNmMtMGI1MTU0NTI0NDRm=";
-        public static string ApiKeyName = "api_key";
-        public static string ApiKey = "curso_api_IlTevUm/z0ey3NwCV/ungWg==";
+        public static string JwtKey;
+        public static string ApiKeyName;
+        public static string ApiKey;
+        public static SmtpConfiguration Smtp = new();
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,50 +1,72 @@
 using Blog;
 using Blog.Data;
+using Blog.Models;
 using Blog.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Blog.Models;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+ConfigureAuthentication(builder);
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
-builder
+var app = builder.Build();
+LoadConfiguration(app);
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+void LoadConfiguration(WebApplication app)
+{
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("Smtp").Bind(smtp);
+    Configuration.Smtp = smtp;
+}
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder
     .Services
     .AddControllers()
     .ConfigureApiBehaviorOptions(options =>
     {
         options.SuppressModelStateInvalidFilter = true;
     });
-builder.Services.AddDbContext<BlogDataContext>();
+}
 
-//AddTransient - toda vez que for invocado o objeto será inicializado
-//AddScoped - o objeto será inicializado uma vez no começo da requisição e finalizado ao final
-//AddSingleton - o objeto será inicializado apenas uma vez e assim ficará por toda a aplicação
-builder.Services.AddTransient<TokenService>();
-builder.Services.AddTransient<PasswordHasher<User>>();
-
-var app = builder.Build();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    //AddTransient - toda vez que for invocado o objeto será inicializado
+    //AddScoped - o objeto será inicializado uma vez no começo da requisição e finalizado ao final
+    //AddSingleton - o objeto será inicializado apenas uma vez e assim ficará por toda a aplicação
+    builder.Services.AddDbContext<BlogDataContext>();
+    builder.Services.AddTransient<TokenService>();
+    builder.Services.AddTransient<PasswordHasher<User>>();
+}
